38f4e127151bf9586c7678a655a019d44d8b25bf
Marcio <marcinhojazz@gmail.com>
2021-10-26 16:24:26 -0300

    components and collection-types to won commerce

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dc6eac7cbe4808e27000559aae1871eb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b90889a8-08d1-4506-87e7-476851f7fc6f'
+};
